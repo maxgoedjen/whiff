29b6fe2257ae1cf37e89adfd9b51d2168905d704
Max Goedjen <max.goedjen@gmail.com>
2022-12-28 12:34:41 -0500

    Show full username

--- Sources/Packages/Sources/TootSniffer/TootSniffer.swift
+++ Sources/Packages/Sources/TootSniffer/TootSniffer.swift
@@ -43,6 +43,11 @@ public final class TootSniffer: TootSnifferProtocol {
         // Gotta be unicode, not utf8
         let attributed = try NSAttributedString(data: raw.content.data(using: .unicode)!, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil)
         var cleaned = raw
+        // Mastodon API doesn't return a clean "@username@example.com" style username, so we'll look at the canonical
+        // url for the
+        if let canonicalServer = URLComponents(url: raw.url, resolvingAgainstBaseURL: true)?.host {
+            cleaned.account.username = "@\(raw.account.username)@\(canonicalServer)"
+        }
         cleaned.content = attributed.string
         // Uncomment to generate JSON for previews
 //        print(String(data: try! JSONEncoder().encode(cleaned), encoding: .utf8)!)
