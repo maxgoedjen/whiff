77a334224275da761da4550d4f188bcf4cc56c1f
Max Goedjen <max.goedjen@gmail.com>
2022-12-29 00:59:51 -0500

    Tests for settings

--- Sources/Packages/Tests/WhiffFeaturesTests/SettingsFeatureTests.swift
+++ Sources/Packages/Tests/WhiffFeaturesTests/SettingsFeatureTests.swift
@@ -1,5 +1,6 @@
 import ComposableArchitecture
 import XCTest
+import SwiftUI
 @testable import WhiffFeatures
 
 @MainActor
@@ -23,6 +24,19 @@ final class SettingsFeatureTests: XCTestCase {
     }
 
     func testLoadFromPartial() async throws {
+        let persistible = SettingsFeature.PersistableState.complete
+        var jsonText = String(data: try JSONEncoder().encode(persistible), encoding: .utf8)!
+        jsonText.replace("showDate", with: "newKey")
+        defaults["settings"] = jsonText.data(using: .utf8)
+        // All the keys in PersistibleState are optional, so both missing and unexpected keys should be fine.
+        await store.send(.load) {
+            $0.textColor = .red
+            $0.linkColor = .blue
+            $0.backgroundColor = .green
+            $0.roundCorners = false
+            $0.imageStyle = .stacked
+            $0.linkStyle = .afterImage
+        }
     }
 
     func testLoadFromInvalidJSON() async throws {
@@ -32,22 +46,22 @@ final class SettingsFeatureTests: XCTestCase {
     }
 
     func testLoadFromComplete() async throws {
-        let persistible = SettingsFeature.PersistableState(
-            textColorData: nil,
-            linkColorData: nil,
-            backgroundColorData: nil,
-            showDate: true,
-            roundCorners: false,
-            imageStyle: .stacked,
-            linkStyle: .afterImage
-        )
-        defaults["settings"] = try JSONEncoder().encode(persistible)
+        defaults["settings"] = SettingsFeature.PersistableState.completeData
         await store.send(.load) {
+            $0.textColor = .red
+            $0.linkColor = .blue
+            $0.backgroundColor = .green
             $0.showDate = true
             $0.roundCorners = false
             $0.imageStyle = .stacked
             $0.linkStyle = .afterImage
         }
+    }
+
+    func testSave() async throws {
+        defaults["settings"] = SettingsFeature.PersistableState.completeData
+        await store.send(.save)
+//        XCTAssertEqual(defaults["settings"], <#T##expression2: Equatable##Equatable#>)
     }
 
     func testDateToggle() async throws {
@@ -60,5 +74,32 @@ final class SettingsFeatureTests: XCTestCase {
         }
         await store.receive(.save)
     }
+
+    func testToggle() async throws {
+        await store.send(.showDateToggled(true)) {
+            $0.showDate = true
+        }
+        await store.receive(.save)
+        await store.send(.showDateToggled(false)) {
+            $0.showDate = false
+        }
+        await store.receive(.save)
+    }
+
+}
+
+extension SettingsFeature.PersistableState {
+
+    static var complete = SettingsFeature.PersistableState(
+        textColorData: Color.red.whf_data,
+        linkColorData: Color.blue.whf_data,
+        backgroundColorData: Color.green.whf_data,
+        showDate: true,
+        roundCorners: false,
+        imageStyle: .stacked,
+        linkStyle: .afterImage
+    )
+
+    static var completeData: Data { try! JSONEncoder().encode(SettingsFeature.PersistableState.complete) }
 
 }
