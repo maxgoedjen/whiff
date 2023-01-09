5d06b9935301f849f3f2f5d724c241f6b6b7457c
Max Goedjen <max.goedjen@gmail.com>
2023-01-08 18:07:36 -0800

    Fix double buttons (h/t @IanKeen

--- Sources/Packages/Sources/WhiffFeatures/AuthenticationFeature.swift
+++ Sources/Packages/Sources/WhiffFeatures/AuthenticationFeature.swift
@@ -3,20 +3,20 @@ import SwiftUI
 import TootSniffer
 
 public struct AuthenticationFeature: ReducerProtocol, Sendable {
-
+    
     @Dependency(\.authenticator) var authenticator
-
+    
     public struct State: Equatable, Sendable {
-
+        
         var loggedIn = false
         var domain: String = ""
         var buttonDisabled = true
-
+        
         public init() {
         }
-
+        
     }
-
+    
     public enum Action: Equatable {
         case onAppear
         case setDomain(String)
@@ -24,10 +24,10 @@ public struct AuthenticationFeature: ReducerProtocol, Sendable {
         case response(TaskResult<String>)
         case logout
     }
-
+    
     public init() {
     }
-
+    
     public var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
@@ -43,9 +43,9 @@ public struct AuthenticationFeature: ReducerProtocol, Sendable {
                     .replacing("http://", with: "")
                     .replacing("https://", with: "")
                 return .task { [host = state.domain] in
-                    .response(await TaskResult {
-                        try await authenticator.obtainOAuthToken(from: host)
-                    })
+                        .response(await TaskResult {
+                            try await authenticator.obtainOAuthToken(from: host)
+                        })
                 }
             case .response(.success):
                 state.loggedIn = true
@@ -60,47 +60,51 @@ public struct AuthenticationFeature: ReducerProtocol, Sendable {
             }
         }
     }
-
+    
 }
 
 public struct AuthenticationFeatureView: View {
-
+    
     let store: StoreOf<AuthenticationFeature>
-
+    
     public init(store: StoreOf<AuthenticationFeature>) {
         self.store = store
     }
-
+    
     public var body: some View {
         WithViewStore(store) { viewStore in
             Group {
                 if viewStore.loggedIn {
-                    Text("You're Logged In")
-                        .font(.title2)
-                    Button("Log Out") {
-                        viewStore.send(.logout)
-                    }
-                    .buttonStyle(BigCapsuleButton())
-                } else {
-                    Form {
-                        Section("Your Mastodon Domain") {
-                            TextField("Your Mastodon Domain", text: viewStore.binding(get: \.domain, send: AuthenticationFeature.Action.setDomain), prompt: Text("mastodon.social"))
-                                .autocorrectionDisabled()
-                                .textInputAutocapitalization(.never)
+                    VStack {
+                        Text("You're Logged In")
+                            .font(.title2)
+                        Button("Log Out") {
+                            viewStore.send(.logout)
                         }
+                        .buttonStyle(BigCapsuleButton())
                     }
-                    Button("Log In") {
-                        viewStore.send(.begin)
+                } else {
+                    VStack {
+                        Form {
+                            Section("Your Mastodon Domain") {
+                                TextField("Your Mastodon Domain", text: viewStore.binding(get: \.domain, send: AuthenticationFeature.Action.setDomain), prompt: Text("mastodon.social"))
+                                    .autocorrectionDisabled()
+                                    .textInputAutocapitalization(.never)
+                            }
+                        }
+                        Button("Log In") {
+                            viewStore.send(.begin)
+                        }
+                        .buttonStyle(BigCapsuleButton())
+                        .disabled(viewStore.buttonDisabled)
                     }
-                    .buttonStyle(BigCapsuleButton())
-                    .disabled(viewStore.buttonDisabled)
                 }
             }
             .padding()
-                .onAppear {
-                    viewStore.send(.onAppear)
-                }
+            .onAppear {
+                viewStore.send(.onAppear)
+            }
         }
     }
-
+    
 }
