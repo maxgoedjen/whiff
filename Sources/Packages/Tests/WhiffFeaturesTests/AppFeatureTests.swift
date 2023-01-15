0a604fa82c755ccd15a96fecc93e82ea214be207
Max Goedjen <max.goedjen@gmail.com>
2023-01-14 18:18:13 -0800

    Test

--- Sources/Packages/Tests/WhiffFeaturesTests/AppFeatureTests.swift
+++ Sources/Packages/Tests/WhiffFeaturesTests/AppFeatureTests.swift
@@ -51,4 +51,36 @@ final class AppFeatureTests: XCTestCase {
         }
     }
 
+    func testAuthFromToolbar() async throws {
+        await store.send(.setShowingAuthentication(.topLevel)) {
+            $0.showingAuthentication = .topLevel
+        }
+    }
+
+    func testAuthFromFailure() async throws {
+        let authenticator = StubAuthenticator(obtainResult: .failure(NotAuthenticatedError()))
+        store = TestStore(
+            initialState: AppFeature.State(showingExport: false),
+            reducer: AppFeature()
+                .dependency(\.tootSniffer, StubTootSniffer(.failure(NotAuthenticatedError()), .failure(NotAuthenticatedError())))
+                .dependency(\.authenticator, authenticator)
+                .dependency(\.keyValueStorage, StubStorage())
+        )
+        await store.send(.load([URL(string: "https://example.com")!])) {
+            $0.showingExport = true
+        }
+        await store.receive(.export(.requested(url: URL(string: "https://example.com")!))) {
+            $0.exportState.lastURL = URL(string: "https://example.com")!
+        }
+        await store.receive(.export(.settings(.load)))
+        await store.receive(.export(.tootSniffCompleted(.failure(NotAuthenticatedError())))) {
+            $0.exportState.errorState = ExportFeature.State.ErrorState(message: NotAuthenticatedError().errorDescription!)
+            $0.exportState.errorState?.button = "Log In"
+        }
+        await store.receive(.export(.tootSniffContextCompleted(.failure(NotAuthenticatedError()))))
+        await store.send(.export(.tappedLogin)) {
+            $0.showingAuthentication = .onExport
+        }
+    }
+
 }
